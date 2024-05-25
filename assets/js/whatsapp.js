b1a2eb2c3d2e79149e42f4e648300d085a0fc3f0
tmerubio <138267089+tmerubio@users.noreply.github.com>
2024-05-25 06:51:55 -0600

    Update whatsapp.js

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -1,13 +1,13 @@
 /* 
-* Author: Gabriel Chávez
-* Website: https://gabrielchavez.me
+* Author: a
+* Website: b
 */
 
 const $form = document.querySelector('#form');
 const buttonSubmit = document.querySelector('#submit');
 const urlDesktop = 'https://web.whatsapp.com/';
 const urlMobile = 'whatsapp://';
-const phone = '524437051162';
+const phone = '7721063775';
 
 
 $form.addEventListener('submit', (event) => {
@@ -19,7 +19,7 @@ $form.addEventListener('submit', (event) => {
         let name = document.querySelector('#name').value
         let lastname = document.querySelector('#lastname').value
         let email = document.querySelector('#email').value
-        let message = 'send?phone=' + phone + '&text=*_Formulario Gabriel Chávez_*%0A*Contacto*%0A%0A*¿Cual es tu nombre?*%0A' + name + '%0A*¿Cuáles son tus apellidos?*%0A' + lastname + '%0A*¿Cuál es tu correo?*%0A' + email + ''
+        let message = 'send?phone=' + phone + '&text=*_Formulario_*%0A*Contacto*%0A%0A*¿Cual es tu nombre?*%0A' + name + '%0A*¿Cuáles son tus apellidos?*%0A' + lastname + '%0A*¿Cuál es tu correo?*%0A' + email + ''
 
 
         if (isMobile()) {
